23ee56cd24ad051799218caea6bed2b68cf446d4
Sebastian Pickl <sebastian.pickl@sz.de>
2019-05-29 10:29:37 +0200

    pass issueKey to SetTransition proc

--- Sources/Procedure/Procedures/SetTransition.swift
+++ Sources/Procedure/Procedures/SetTransition.swift
@@ -3,10 +3,14 @@ import Request
 import UI
 
 public struct SetTransition: Procedure {
-    public init() {}
+    private let issueKey: String?
+
+    public init(issueKey: String? = Env.current.jira.currentIssueKey()) {
+        self.issueKey = issueKey
+    }
 
     public func run() -> Bool {
-        guard let issueKey = Env.current.jira.currentIssueKey(),
+        guard let issueKey = issueKey,
             let response = GetIssueTransitions(issueKey: issueKey).awaitResponseWithDebugPrinting(),
             let transition = LineSelector(dataSource: GenericLineSelectorDataSource(items: response.transitions, line: \.name))?.singleSelection()?.output,
             PostTransition(issueKey: issueKey, transitionId: transition.id).awaitResponseWithDebugPrinting() != nil
