18c82404015c5da1d01a98703cd0d092d44dd0c2
Sebastian Pickl <sebastian.pickl@sz.de>
2019-09-04 15:00:53 +0200

    reduce number of commits to fetch

--- Sources/Procedure/Procedure.swift
+++ Sources/Procedure/Procedure.swift
@@ -38,7 +38,7 @@ extension Procedure {
     }
 
     func getCommitters(stashProject: String, repo: String) -> Future<Result<[User], Error>> {
-        return GetLastCommits(stashProject: stashProject, repo: repo, limit: 500)
+        return GetLastCommits(stashProject: stashProject, repo: repo, limit: 250)
             .request()
             .map { result -> Result<[User], Error> in
                 result.map { response -> [User] in
