d32439699a739b3dc6e493420bbf9ae25475401b
Sebastian Pickl <sebastian.pickl@sz.de>
2019-09-06 13:25:22 +0200

    query last 1000 commits to ensure to get all relevant committers

--- Sources/Procedure/Procedure.swift
+++ Sources/Procedure/Procedure.swift
@@ -38,7 +38,7 @@ extension Procedure {
     }
 
     func getCommitters(stashProject: String, repo: String) -> Future<Result<[User], Error>> {
-        return GetLastCommits(stashProject: stashProject, repo: repo, limit: 250)
+        return GetLastCommits(stashProject: stashProject, repo: repo, limit: 1000)
             .request()
             .map { result -> Result<[User], Error> in
                 result.map { response -> [User] in
